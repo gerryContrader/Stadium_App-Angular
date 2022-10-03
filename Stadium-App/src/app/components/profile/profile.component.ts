01d41717086805e89a0b1d9267fc56959f547c94
nicolo-dalessandro-contrader <n.dalessandro@contrader.it>
2022-10-04 00:20:31 +0200

    refactoring user profile

--- Stadium-App/src/app/components/profile/profile.component.ts
+++ Stadium-App/src/app/components/profile/profile.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
+import { TicketService } from 'src/app/services/ticket.service';
 import { UserDTO } from 'src/models/userdto';
+import { TicketDTO } from 'src/models/ticketdto';
+import { FavouriteDTO } from 'src/models/favouritedto';
+import { FavouriteService } from 'src/app/services/favourite.service';
 
 @Component({
   selector: 'app-profile',
@@ -10,17 +14,31 @@ import { UserDTO } from 'src/models/userdto';
 })
 export class ProfileComponent implements OnInit {
   ciao:String = "ciao"
+  public tickets!: TicketDTO[];
+  public favourites!: FavouriteDTO[];
 
   public users: UserDTO = new UserDTO();
-  constructor(public service: UserService,private router: Router, private actRoute: ActivatedRoute,) { }
+  constructor(public service: UserService, public favouriteService:FavouriteService, public ticketService:TicketService, private router: Router, private actRoute: ActivatedRoute,) { }
   public  userid:number;
   user!:UserDTO;
   propicSrc!:String;
   ngOnInit(): void {
     this.user = JSON.parse(localStorage.getItem('currentUser') as string);
     this.propicSrc = "data:image/png;base64,"+ this.user.propic;
-    /* console.log(this.propicSrc); */
-    console.log(this.user.propic);
+    this.ticketService.getAllByUserId(this.user.id).subscribe(x => {
+
+      this.tickets = x;
+
+    });
+    this.favouriteService.getAllByUserId(this.user.id).subscribe(x =>{
+      this.favourites = x;
+    })
+  }
+
+  deleteFavourite(favourite: FavouriteDTO){
+    this.favouriteService.delete(favourite.id).subscribe(() => {
+      this.router.navigate(['profile'])
+    });
   }
 
 }
