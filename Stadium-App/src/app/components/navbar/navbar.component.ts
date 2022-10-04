afa6ad6647023cc27037832fc61f36e21e9c451a
Gerardo <gerardo.dbl@gmail.com>
2022-10-04 16:26:24 +0200

    adjust  styling in navbar component

--- Stadium-App/src/app/components/navbar/navbar.component.ts
+++ Stadium-App/src/app/components/navbar/navbar.component.ts
@@ -9,11 +9,22 @@ import { UserDTO } from 'src/models/userdto';
 export class NavbarComponent implements OnInit {
 
   public user!: UserDTO;
+  public role!: number;
+  public propicSrc!: string;
 
   constructor() { }
 
   ngOnInit(): void {
     this.user = JSON.parse(localStorage.getItem('currentUser') as string);
+    this.propicSrc = "data:image/png;base64,"+ this.user.propic;
+    if (this.user.usertype === "USER") {
+      this.role = 2;
+    } else if (this.user.usertype === "ADMIN") {
+      this.role = 1;
+    }
+    else {
+      this.role = 0;
+    }
   }
 
 }
